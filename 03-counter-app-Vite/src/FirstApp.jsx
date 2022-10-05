bd1476e288f548883cdc69fa6ae7d864caa913d4
Byron Tercero <B1tercero.cruz@gmail.com>
2022-10-04 21:27:26 -0600

    print html variables

--- 03-counter-app-Vite/src/FirstApp.jsx
+++ 03-counter-app-Vite/src/FirstApp.jsx
@@ -1,8 +1,17 @@
 
+// const newMessage = {
+//     message: 'Hola mundo',
+//     title: 'Fernando'
+// };
+
+
+const persona = (nombre, apellido ) => (`Hola soy ${nombre} ${apellido} mucho gusto`);
+
 export const FirtsApp = () =>{
+
     return(
-        <>
-            <h2>Firs <span>App</span></h2>
+        <> 
+            <h1>{ persona('Byron','Tercero') }</h1>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro voluptas dicta est iste sit a illo voluptatibus atque odit impedit sint alias suscipit placeat magni ratione, earum laboriosam debitis ipsum!</p>
         </>
     )
