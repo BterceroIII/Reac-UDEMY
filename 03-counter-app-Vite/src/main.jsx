8a9005fa3b2847365c3e53201922c68384207a95
Byron Tercero <B1tercero.cruz@gmail.com>
2022-10-04 21:44:27 -0600

    add css styles

--- 03-counter-app-Vite/src/main.jsx
+++ 03-counter-app-Vite/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { FirtsApp } from './FirstApp';
 import { HelloWordApp} from './HelloWordApp';
 
-
+import './style.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
